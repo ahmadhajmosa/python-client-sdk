9a36f7f29e6b5ba7f1edafdbabcb8a28fdb208a7
Soren <s.madsen@cept.at>
2015-02-18 18:50:52 +0100

    Fixed return type for /expressions. Improved tests.

--- python-client-sdk/tests/testExpressionsApi.py
+++ python-client-sdk/tests/testExpressionsApi.py
@@ -27,14 +27,14 @@ class TestClientExpreissions(unittest.TestCase):
         
     def testExpressions(self):
         fp = self.api.resolveExpression(conf.RETINA_NAME, oneTermInputJSON, sparsity=0.5)
-        self.assertTrue(len(fp.positions) > 0)
+        self.assertGreater(len(fp.positions), 100)
 
     def testContexts(self):
         contexts = self.api.getContextsForExpression(conf.RETINA_NAME, oneTermInputJSON, get_fingerprint=True, start_index=0, max_results=3, sparsity=1.0)
         self.assertTrue(contexts != None)
         self.assertEqual(3, len(contexts))
         c0 = contexts[0]
-        self.assertTrue( len(c0.fingerprint.positions) > 0)
+        self.assertGreater(len(c0.fingerprint.positions), 100)
         self.assertTrue(isinstance(c0.context_label, unicode))
         self.assertTrue(c0.context_id == 0)
 
@@ -43,21 +43,21 @@ class TestClientExpreissions(unittest.TestCase):
         self.assertTrue(terms != None)
         self.assertEqual(8, len(terms))
         t0 = terms[0]
-        self.assertTrue(len(t0.fingerprint.positions) > 0)
+        self.assertGreater(len(t0.fingerprint.positions), 100)
         self.assertTrue(t0 != None)
 
     def testExpressionBulk(self):
         fps = self.api.resolveBulkExpression(conf.RETINA_NAME, self.jsonBulkExpression)
         self.assertEqual(6, len(fps))
         for fp in fps:
-            self.assertTrue(len(fp.positions) > 0)
+            self.assertGreater(len(fp.positions), 50)
 
     def testExpressionContextsBulk(self):
         contextsList = self.api.getContextsForBulkExpression(conf.RETINA_NAME, self.jsonBulkExpression, get_fingerprint=True, start_index=0, max_results=3)
         self.assertEqual(len(contextsList), 6)
         for contextList in contextsList:
             for i, context in enumerate(contextList):
-                self.assertTrue(len(context.fingerprint.positions) > 0)
+                self.assertGreater(len(context.fingerprint.positions), 50)
                 self.assertTrue(isinstance(context.context_label, unicode))
                 self.assertTrue(context.context_id == i)
 
@@ -66,7 +66,7 @@ class TestClientExpreissions(unittest.TestCase):
         self.assertTrue(len(termsLists) == 6)
         for termList in termsLists:
             self.assertTrue(len(termList) == 7)
-            self.assertTrue(len(termList[0].fingerprint.positions) > 0)
+            self.assertGreater(len(termList[0].fingerprint.positions), 100)
 
 if __name__ == "__main__":
     unittest.main()
